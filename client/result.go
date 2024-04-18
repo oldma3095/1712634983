267b720bc18100e2fc813eaacc9d11e296c7adac
ma3315865 <ma3315865@outlook.com>
2024-04-18 10:57:06 +0800

    test

--- client/result.go
+++ client/result.go
@@ -2,17 +2,37 @@ package client
 
 import (
 	"context"
+	"github.com/oldma3095/1712634983/cache"
 	commonApi "github.com/oldma3095/1712634983/protos/common/api"
 	"net/http"
 )
 
-func (client *Clients) PushResultToMaster(data *commonApi.Result, predictErr error) {
+func (client *Clients) PushResultToMaster(data cache.NiuNiuResult, predictErr error) {
 	req := &commonApi.ResultReq{}
 	if predictErr != nil {
 		req.Code = http.StatusInternalServerError
 		req.Msg = predictErr.Error()
 	} else {
-		req.Data = data
+		req.Data = &commonApi.Result{
+			Id:            data.Id,
+			SaveTime:      data.SaveTime,
+			Image:         data.Image,
+			RawImage:      data.RawImage,
+			Video:         data.Video,
+			VideoSize:     data.VideoSize,
+			Flag:          data.Flag,
+			Banker:        data.Banker,
+			Player1:       data.Player1,
+			Player2:       data.Player2,
+			Player3:       data.Player3,
+			Other:         data.Other,
+			SimpleFlag:    data.SimpleFlag,
+			SimpleBanker:  data.SimpleBanker,
+			SimplePlayer1: data.SimplePlayer1,
+			SimplePlayer2: data.SimplePlayer2,
+			SimplePlayer3: data.SimplePlayer3,
+			SimpleOther:   data.SimpleOther,
+		}
 	}
 
 	res, err := client.apiServiceClient.Result(context.Background(), req)
--- test/grpc_server_test.go
+++ test/grpc_server_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/oldma3095/1712634983/client"
 	"github.com/oldma3095/1712634983/server"
 	"testing"
+	"time"
 )
 
 func TestGrpcServer(t *testing.T) {
@@ -15,5 +16,18 @@ func TestGrpcClient(t *testing.T) {
 	ip := "127.0.0.1"
 	port := 7555
 
-	client.NewMaster(ip, port)
+	c, err := client.NewMaster(ip, port)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		for {
+			select {
+			case <-ticker.C:
+				c.PushClientInfoToMaster()
+			}
+		}
+	}()
+	c.PushResultToMaster()
 }
