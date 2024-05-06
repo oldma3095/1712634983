e7c80bc4e3808fb6bcfb291a868450a5b48e9756
ma3315865 <ma3315865@outlook.com>
2024-05-06 16:56:48 +0800

    grpc to rpcx

--- slave/handle.go
+++ slave/handle.go
@@ -1,7 +1,10 @@
 package slave
 
+import "github.com/oldma3095/1712634983/cache"
+
 type Handle struct {
-	Code int32      `json:"code"`
-	Msg  string     `json:"msg"`
-	Data ResultData `json:"data"`
+	Code int32              `json:"code"`
+	Msg  string             `json:"msg"`
+	Data cache.NiuNiuResult `json:"data"`
+	Init bool               `json:"init"` // 首次连接初始化
 }
