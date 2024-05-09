4b9b6ed77d4dae5ea33ea3f3ba4fdf0c3c481940
ma3315865 <ma3315865@outlook.com>
2024-05-09 10:16:15 +0800

    grpc to rpcx

--- slave/handle.go
+++ slave/handle.go
@@ -3,9 +3,10 @@ package slave
 import "github.com/oldma3095/1712634983/cache"
 
 type Handle struct {
-	Code int32              `json:"code"`
-	Msg  string             `json:"msg"`
-	Data cache.NiuNiuResult `json:"data"`
-	Init bool               `json:"init"` // 首次连接初始化
-	UUID string             `json:"uuid"`
+	Code          int32              `json:"code"`
+	Msg           string             `json:"msg"`
+	Data          cache.NiuNiuResult `json:"data"`
+	Init          bool               `json:"init"` // 首次连接初始化
+	UUID          string             `json:"uuid"`
+	RecordFileUrl string             `json:"recordFileUrl"` // 录制后返回的视频地址
 }
