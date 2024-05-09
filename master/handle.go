acc895831777e8d28a5addd2b89c4a79b5f59ab2
ma3315865 <ma3315865@outlook.com>
2024-05-09 10:45:23 +0800

    grpc to rpcx

--- master/handle.go
+++ master/handle.go
@@ -1,6 +1,7 @@
 package master
 
-type Handle struct {
-	Method string `json:"method"`           // create delete update get
-	Record bool   `json:"record,omitempty"` // 是否录制视频
+type HandlePredict struct {
+}
+
+type HandleRecord struct {
 }
--- slave/handle.go
+++ slave/handle.go
@@ -3,10 +3,16 @@ package slave
 import "github.com/oldma3095/1712634983/cache"
 
 type Handle struct {
-	Code          int32              `json:"code"`
-	Msg           string             `json:"msg"`
-	Data          cache.NiuNiuResult `json:"data"`
-	Init          bool               `json:"init"` // 首次连接初始化
-	UUID          string             `json:"uuid"`
-	RecordFileUrl string             `json:"recordFileUrl"` // 录制后返回的视频地址
+	Init bool   `json:"init"` // 首次连接初始化
+	UUID string `json:"uuid"`
+}
+
+type HandlePredict struct {
+	Code int32              `json:"code"`
+	Msg  string             `json:"msg"`
+	Data cache.NiuNiuResult `json:"data"`
+}
+
+type HandleRecord struct {
+	RecordFileUrl string `json:"recordFileUrl"` // 录制后返回的视频地址
 }
