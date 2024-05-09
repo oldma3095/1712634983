de9bf3212dbe0fb4c123da58131c411bb62e5800
ma3315865 <ma3315865@outlook.com>
2024-05-09 10:15:02 +0800

    grpc to rpcx

--- enum/handle_method.go
+++ enum/handle_method.go
@@ -1,8 +1,10 @@
 package enum
 
 const (
-	HandleMethodCreate = "create"
-	HandleMethodDelete = "delete"
-	HandleMethodUpdate = "update"
-	HandleMethodGet    = "get"
+	HandleMethodCreate  = "create"
+	HandleMethodDelete  = "delete"
+	HandleMethodUpdate  = "update"
+	HandleMethodGet     = "get"
+	HandleMethodPredict = "predict" // 检测返回结果
+	HandleMethodRecord  = "record"  // 录制视频
 )
